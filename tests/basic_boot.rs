61f42f7aba245764587b0fdaa4128028b799d617
Brandon Chinn <brandonchinn178@gmail.com>
2023-09-21 20:38:37 -0700

    Break out lib.rs, add integration test

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-
 #![feature(custom_test_frameworks)]
 #![test_runner(toy_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -11,28 +10,18 @@ use toy_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World! {}", ":)");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     toy_os::test_panic_handler(info)
 }
 
 #[test_case]
-fn smoketest() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
+
